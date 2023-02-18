2462f7b25c527e704f258d4123c2d8f9b9091a40
koryo <koryobewakwakcom@gmail.com>
2023-02-18 23:28:32 +0900

    押した文字が赤になるように変更

--- type.js
+++ type.js
@@ -19,7 +19,8 @@ function push_Keydown(event){
 	else {
 		let Q_length = Q[Q_No].length;
 		if (Q[Q_No].charAt(Q_index) == keyCode) { //押したキーが合っていたら
-			Q_index++; //判定する文章に１足す
+			Q_index++;
+			document.getElementById("done").innerHTML = Q[Q_No].substring(0, Q_index);
 			typeword.innerHTML = Q[Q_No].substring(Q_index, Q_length); //問題を書き出す
 			if (Q_length == Q_index){ //全部正解したら
 				new Audio('audio/sucess.mp3').play();
@@ -33,4 +34,5 @@ function createNewType(typeword) {
 	Q_No = Math.floor( Math.random() * Q.length);//問題をランダムで出題する
 	Q_index = 0;
 	typeword.innerHTML = Q[Q_No];
+	document.getElementById("done").innerHTML = "";
 }

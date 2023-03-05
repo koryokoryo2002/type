67ad8fb80ed89a787f4350c0dfcfc3fafa6c9d2f
koryo <koryobewakwakcom@gmail.com>
2023-03-05 12:53:30 +0900

    リファクタリング

--- type.js
+++ type.js
@@ -6,22 +6,25 @@ let Q_index = -1;
 window.addEventListener("keydown", push_Keydown);
 
 function push_Keydown(event){
-	new Audio('audio/click.mp3').play(); //audio key click
+	new Audio('audio/click.mp3').play();				//audio key click
 
 	let typingTextDispArea = document.getElementById("start")
 	let keyCode = event.key;
 
 
-	if(Q_index == -1) {
+	if(Q_index == -1) {									//page is loaded
 		createNewType(typingTextDispArea)
 	}
 	else {
 		let text_length = typingText.length;
-		if (typingText.charAt(Q_index) == keyCode) { //押したキーが合っていたら
+		if (typingText.charAt(Q_index) == keyCode) {	//press key is correct
 			Q_index++;
-			document.getElementById("done").innerHTML = typingText.substring(0, Q_index);
-			typingTextDispArea.innerHTML = typingText.substring(Q_index, text_length); //問題を書き出す
-			if (text_length == Q_index){ //全部正解したら
+			document.getElementById("done").innerHTML
+				= typingText.substring(0, Q_index);		//pressed key become red
+			typingTextDispArea.innerHTML
+				= typingText.substring(Q_index, text_length);
+			
+			if (text_length == Q_index){				// complete typing
 				new Audio('audio/sucess.mp3').play();
 				createNewType(typingTextDispArea)
 			}
@@ -30,7 +33,7 @@ function push_Keydown(event){
 }
 
 function createNewType(typingTextDispArea) {
-	typingText = Q[Math.floor( Math.random() * Q.length)];//問題をランダムで出題する
+	typingText = Q[Math.floor( Math.random() * Q.length)];
 	Q_index = 0;
 	typingTextDispArea.innerHTML = typingText;
 	document.getElementById("done").innerHTML = "";
